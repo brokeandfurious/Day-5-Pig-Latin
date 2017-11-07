0ff968b2e9a20f73ec49630aa296abc73ea61343
brokeandfurious <forumbomb@gmail.com>
2017-11-07 13:59:11 -0500

    Initial Commit

---
+++ Day-5-Pig-Latin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Day-5-Pig-Latin
+//
+//  Created by Murat Ekrem Kolcalar on 11/7/17.
+//  Copyright © 2017 mumu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
